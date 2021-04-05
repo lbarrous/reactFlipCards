540452ccb218e2fe6badef3d25e303f8a15fc08e
Alvaro Perez Garcia <apgarcia@MacBook-Pro-de-Alvaro.local>
2021-04-06 01:51:49 +0200

    Saved store on localStorage

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,9 +1,35 @@
 import { createStore } from "redux";
 import rootReducer from "./Reducers/rootReducer";
 
+// convert object to string and store in localStorage
+function saveToLocalStorage(state: AppState) {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+  
+  // load string from localStarage and convert into an Object
+  // invalid output must be undefined
+  function loadFromLocalStorage() {
+    try {
+      const serialisedState = localStorage.getItem("persistantState");
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+
 const store = createStore(
-  rootReducer
+  rootReducer,
+  loadFromLocalStorage()
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export type AppState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
